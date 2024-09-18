cf4647054f162a52748fe843756b6fdf7a8969bd
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-09-18 20:53:06 +0900

    Update analog.sh

--- analog.sh
+++ analog.sh
@@ -1,69 +1,103 @@
 #!/bin/bash
 
-# 색깔 변수 정의
-RED='\033[0;31m'
+# 굵은 글씨와 색상 정의
+BOLD="\033[1m"
 GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+NC='\033[0m'
 
-echo -e "${GREEN}Sonic 데일리 퀘스트 스크립트를 시작합니다...${NC}"
+# 시스템 종속성 업데이트
+echo -e "${BLUE}시스템 종속성을 업데이트 중입니다...${NC}"
+echo
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+echo
 
-# 작업 디렉토리 설정
-work="/root/sonic-all"
-
-# 기존 작업 디렉토리가 존재하면 삭제
-if [ -d "$work" ]; then
-    echo -e "${YELLOW}작업 디렉토리 '${work}'가 이미 존재하므로 삭제합니다.${NC}"
-    rm -rf "$work"
-fi
-
-# 파일 다운로드 및 덮어쓰기
-echo -e "${YELLOW}필요한 파일들을 다운로드합니다...${NC}"
-
-# Git 설치
-echo -e "${YELLOW}Git을 설치합니다...${NC}"
-sudo apt install -y git
-
-# 존재하는 파일을 삭제하고 다운로드
-echo -e "${YELLOW}Git 저장소 클론 중...${NC}"
-rm -rf ./*
-git clone https://github.com/KangJKJK/sonic-all
-
-# 작업 디렉토리 이동
-echo -e "${YELLOW}작업디렉토리를 이동합니다...${NC}"
-cd "$work"
-
-# npm 설치 여부 확인
-echo -e "${YELLOW}필요한 파일들을 설치합니다...${NC}"
-if ! command -v npm &> /dev/null; then
-    echo -e "${RED}npm이 설치되지 않았습니다. npm을 설치합니다...${NC}"
-    sudo apt-get update
-    sudo apt-get install -y npm
+# 도커 설치 확인
+echo -e "${BOLD}${CYAN}Docker 설치 확인 중...${NC}"
+if command -v docker >/dev/null 2>&1; then
+    echo -e "${GREEN}Docker가 이미 설치되어 있습니다.${NC}"
 else
-    echo -e "${GREEN}npm이 이미 설치되어 있습니다.${NC}"
+    echo -e "${RED}Docker가 설치되어 있지 않습니다. Docker를 설치하는 중입니다...${NC}"
+    sudo apt update && sudo apt install -y curl net-tools
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    echo -e "${GREEN}Docker가 성공적으로 설치되었습니다.${NC}"
 fi
 
-# Node.js 모듈 설치
-echo -e "${YELLOW}필요한 Node.js 모듈을 설치합니다...${NC}"
-npm install
-npm install @solana/web3.js chalk bs58
+# Docker 설치 확인
+echo -e "${BLUE}Docker 설치를 확인 중입니다...${NC}"
+echo
+sudo docker run hello-world
+echo
 
-# 개인키 입력받기
-read -p "Solana의 개인키를 쉼표로 구분하여 입력하세요: " privkeys
+# Analog Timechain Docker 이미지 가져오기
+echo -e "${BLUE}Analog Timechain Docker 이미지를 가져오는 중입니다...${NC}"
+echo
+docker pull analoglabs/timechain
+echo
 
-# 개인키를 파일에 저장
-echo "$privkeys" > "$work/private.txt"
+# NODE_NAME 변수 설정
+echo -e "${BLUE}NODE_NAME 변수를 설정 중입니다...${NC}"
+echo
+read -p "노드에 사용할 이름을 입력하세요: " NODE_NAME
+echo "export NODE_NAME=\"$NODE_NAME\"" >> ~/.bash_profile
+source ~/.bash_profile
+echo
+echo -e "${BLUE}노드 이름을 기억해야 하며, 화이트리스트 폼 제출 시 필요합니다.${NC}"
+echo
 
-# 파일 생성 확인
-if [ -f "$work/private.txt" ]; then
-    echo -e "${GREEN}개인키 파일이 성공적으로 생성되었습니다.${NC}"
-else
-    echo -e "${RED}개인키 파일 생성에 실패했습니다.${NC}"
+# Analog Timechain Docker 컨테이너 실행
+echo -e "${BLUE}Analog Timechain Docker 컨테이너를 실행 중입니다...${NC}"
+echo
+docker run -d --name analog -p 9944:9944 -p 30303:30303 analoglabs/timechain \
+    --base-path /data \
+    --rpc-external \
+    --unsafe-rpc-external \
+    --rpc-cors all \
+    --name $NODE_NAME \
+    --telemetry-url="wss://telemetry.analog.one/submit 9" \
+    --rpc-methods Unsafe
+echo
+
+# websocat 설치
+echo -e "${BLUE}websocat을 설치 중입니다...${NC}"
+sudo wget -qO /usr/local/bin/websocat https://github.com/vi/websocat/releases/latest/download/websocat.x86_64-unknown-linux-musl
+sudo chmod a+x /usr/local/bin/websocat
+echo
+
+# websocat 버전 확인
+echo -e "${BLUE}websocat 버전을 확인 중입니다...${NC}"
+echo
+websocat --version
+if [ $? -ne 0 ]; then
+    echo "websocat 설치 실패 또는 경로에 없습니다."
+    exit 1
 fi
+echo
 
-# sonic.js 스크립트 실행
-echo -e "${GREEN}sonic.js 스크립트를 실행합니다...${NC}"
-node --no-deprecation sonic.js
+# 잠시 대기
+sleep 5
 
+# jq 설치
+sudo apt-get install jq
+
+# 잠시 대기
+sleep 2
+
+# websocat을 사용하여 로테이션키를 생성중입니다.
+echo -e "${BLUE}websocat을 사용하여 로테이션키를 생성중입니다...${NC}"
+echo
+RESPONSE=$(echo '{"id":1,"jsonrpc":"2.0","method":"author_rotateKeys","params":[]}' | websocat -n1 -B 99999999 ws://127.0.0.1:9944)
+if [ $? -ne 0 ]; then
+    echo "websocat을 사용하여 로테이션키를 생성하는데 실패하였습니다."
+    exit 1
+fi
+KEY=$(echo $RESPONSE | jq -r '.result')
+echo -e "로테이션키는 다음과 같습니다: ${GREEN}$KEY${NC}"
+echo
+
+# 작업 완료 메시지
 echo -e "${GREEN}모든 작업이 완료되었습니다. 컨트롤+A+D로 스크린을 종료해주세요.${NC}"
 echo -e "${GREEN}스크립트 작성자: https://t.me/kjkresearch${NC}"
